c75af04517f707841f74bd206c04c885d436d87f
Yi Mao <ymao@otter.ai>
2025-12-01 21:10:15 -0800

    update adapters

--- tests/adapters.py
+++ tests/adapters.py
@@ -452,7 +452,9 @@ def run_cross_entropy(
     raise NotImplementedError
 
 
-def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float) -> None:
+def run_gradient_clipping(
+    parameters: Iterable[torch.nn.Parameter], max_l2_norm: float
+) -> None:
     """Given a set of parameters, clip their combined gradients to have l2 norm at most max_l2_norm.
 
     Args:
@@ -589,4 +591,6 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    from bpe.bpe import run_train_bpe
+
+    return run_train_bpe(input_path, vocab_size, special_tokens)

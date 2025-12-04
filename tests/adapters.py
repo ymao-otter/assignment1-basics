00c2a5e3ff1f4b55e0b649995a330e56a6502c8a
Yi Mao <ymao@otter.ai>
2025-12-04 08:09:18 -0800

    implement linear module

--- tests/adapters.py
+++ tests/adapters.py
@@ -28,8 +28,11 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
+    from transformer.linear import Linear
 
-    raise NotImplementedError
+    linear = Linear(d_in, d_out)
+    linear.load_state_dict({"weight": weights})
+    return linear(in_features)
 
 
 def run_embedding(

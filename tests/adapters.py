c192da3d9e4e299650a6b8659c571525ec838380
Yi Mao <ymao@otter.ai>
2025-12-03 20:38:11 -0800

    adapters

--- tests/adapters.py
+++ tests/adapters.py
@@ -561,7 +561,9 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    from bpe.bpe_tokenizer import Tokenizer
+
+    return Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(

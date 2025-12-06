a047181a0f2511bb36cad5f477e0e08bfc3d8a87
Yi Mao <ymao@otter.ai>
2025-12-06 12:36:15 -0800

    init rope

--- tests/adapters.py
+++ tests/adapters.py
@@ -213,7 +213,10 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    from transformer.rope import RoPE
+
+    rope = RoPE(theta, d_k, max_seq_len)
+    return rope(in_query_or_key)[:, token_positions, :]
 
 
 def run_transformer_block(

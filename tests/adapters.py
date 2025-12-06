da482d0d895cc13c87017ffe81b33debfc853adf
Yi Mao <ymao@otter.ai>
2025-12-06 13:07:39 -0800

    fix softmax

--- tests/adapters.py
+++ tests/adapters.py
@@ -451,7 +451,9 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    from transformer.softmax import softmax
+
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(

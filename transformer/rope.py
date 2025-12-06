a14760ec921767c970df09b8327617d9f712aa8f
Yi Mao <ymao@otter.ai>
2025-12-06 12:39:59 -0800

    fix rope

--- transformer/rope.py
+++ transformer/rope.py
@@ -13,22 +13,23 @@ class RoPE(nn.Module):
         self.max_seq_len = max_seq_len
         self.device = device
         self.register_buffer(
-            "blocks", torch.empty(max_seq_len, d_k, 2, 2, device=device)
+            "blocks", torch.empty(max_seq_len, d_k // 2, 2, 2, device=device)
         )
         self.reset_blocks()
 
     def reset_blocks(self):
         for i in range(self.max_seq_len):
-            for k in range(self.d_k):
+            for k in range(self.d_k // 2):
                 l_theta = i / self.theta ** (2 * k / self.d_k)
-                self.blocks[i, k, 0, 0] = torch.cos(l_theta)
-                self.blocks[i, k, 0, 1] = -torch.sin(l_theta)
-                self.blocks[i, k, 1, 0] = torch.sin(l_theta)
-                self.blocks[i, k, 1, 1] = torch.cos(l_theta)
+                l_theta_tensor = torch.tensor(l_theta, device=self.device)
+                self.blocks[i, k, 0, 0] = torch.cos(l_theta_tensor)
+                self.blocks[i, k, 0, 1] = -torch.sin(l_theta_tensor)
+                self.blocks[i, k, 1, 0] = torch.sin(l_theta_tensor)
+                self.blocks[i, k, 1, 1] = torch.cos(l_theta_tensor)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         seq_len = x.shape[-2]
         *leading, d = x.shape
         x_blocks = x.view(*leading, d // 2, 2)
         val = einops.einsum(x_blocks, self.blocks[:seq_len], "... l, ... j l -> ... j")
-        return val.view(x.shape)
+        return val.reshape(x.shape)
